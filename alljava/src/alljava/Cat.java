389ced819018994bfaa235f3a69002102ff14e49
emanname <36789944+emanname@users.noreply.github.com>
2018-02-27 22:28:16 +0200

    Finished video
    i playedd with almoust all staff in video so this branch can be merged to main.

--- alljava/src/alljava/Cat.java
+++ alljava/src/alljava/Cat.java
@@ -1,5 +1,29 @@
 package alljava;
 
 public class Cat extends Animal{
+	
+	public Cat() {
+		
+		//cat do nothing
+		
+	}
+//25:30
+	
+	
+	public String makeSound() {
+		
+		return "Myaaau";
+		
+	}
+	
+	public static void main(String[] args) {
+		
+		Animal doggy = new Dog();
+		Animal kitty = new Cat();
+		
+		System.out.println("doggy says " + doggy.makeSound() + " and kitty says " + kitty.makeSound() );
+		//test of speak animal
+		speakAnimal(doggy);
 
+	}
 }
--- alljava/src/alljava/Dog.java
+++ alljava/src/alljava/Dog.java
@@ -1,5 +1,16 @@
 package alljava;
 
 public class Dog extends Animal{
-
+	
+	public Dog() {
+		//dog do nothing at create
+	}
+	
+	public String makeSound() {
+		
+		return "Wooof";
+		
+	}
+	
 }
+
